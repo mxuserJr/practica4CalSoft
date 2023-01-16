e15f835d584f1a47b20f73cc61b8d406eca51b98
rodrigollamasmartinez <rodrigollamasmartinez@192.168.1.38>
2023-01-16 22:53:16 +0100

    Initial Commit

---
+++ practica4CS/src/main/java/com/calidadDelSoftware/practica4CS/App.java
@@ -0,0 +1,13 @@
+package com.calidadDelSoftware.practica4CS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ practica4CS/src/main/java/com/calidadDelSoftware/practica4CS/Persona.java
@@ -0,0 +1,10 @@
+package com.calidadDelSoftware.practica4CS;
+
+public class Persona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
